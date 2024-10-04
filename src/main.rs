7c280d4ad198e1f3d9ebd0d139636586889dbe50
unknown <devingforart@gmail.com>
2024-10-04 16:32:09 -0300

    cors

--- src/main.rs
+++ src/main.rs
@@ -342,11 +342,17 @@ async fn main() -> std::io::Result<()> {
             .app_data(db.clone())
             .wrap(
                 Cors::default()
-                .allowed_origin("https://test.devingfor.art")  // Permitir solicitudes desde test.devingfor.art
-                .allowed_origin("https://devingfor.art")  // Permitir solicitudes desde devingfor.art
-                            .allowed_methods(vec!["GET", "POST", "DELETE", "OPTIONS"]) // Permitir métodos específicos
-                    .allowed_headers(vec![http::header::CONTENT_TYPE, http::header::AUTHORIZATION, http::header::ACCEPT])
-                    .allow_any_header()  // Permitir cualquier encabezado en las solicitudes
+                    .allowed_origin_fn(|origin, _req_head| {
+                        origin.as_bytes().ends_with(b"test.devingfor.art")
+                            || origin.as_bytes().ends_with(b"devingfor.art")
+                    })
+                    .allowed_methods(vec!["GET", "POST", "DELETE", "OPTIONS"]) // Permitir métodos específicos
+                    .allowed_headers(vec![
+                        http::header::CONTENT_TYPE,
+                        http::header::AUTHORIZATION,
+                        http::header::ACCEPT,
+                    ])
+                    .allow_any_header() // Permitir cualquier encabezado en las solicitudes
                     .supports_credentials() // Permitir el uso de cookies y credenciales en las solicitudes de CORS
                     .max_age(3600), // Cachea la respuesta preflight por 3600 segundos
             )
